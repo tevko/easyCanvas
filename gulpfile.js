eecae38a4d8ad0cc64dec4a7b8fbc320110044e8
tevko <timevko@gmail.com>
2015-05-01 15:05:12 -0400

    more setup stuff

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+// Include gulp and plugins
+var gulp = require('gulp'),
+	rename = require('gulp-rename'),
+	clean = require('gulp-clean'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	jshint = require('gulp-jshint');
+
