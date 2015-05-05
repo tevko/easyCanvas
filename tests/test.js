4b90902c13b4fbcf4df66d5477e7a360b961930e
Tim <timevko@gmail.com>
2015-05-04 20:48:27 -0400

    first test

---
+++ tests/test.js
@@ -0,0 +1,6 @@
+describe('Check easyCanvas Object', function() {
+    var test = easyCanvas.create('.test', '2d');
+    it('Should return an Object', function() {
+        expect(typeof a).toBe('object');
+    });
+});
