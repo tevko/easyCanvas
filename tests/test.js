a44962838b8faa49c496cc0a050d103ca13a97a5
Tim <timevko@gmail.com>
2015-05-04 21:43:24 -0400

    'testing stuff

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,7 @@
 describe('Check easyCanvas Object', function() {
+    var t = document.createElement('canvas');
+    t.classList.add('test');
+    document.body.appendChild(t);
     var test = easyCanvas.create('.test', '2d');
     it('Should return an Object', function() {
         expect(typeof test).toBe('object');
