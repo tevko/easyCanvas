05cbf3c870bc85ffb67c9aa2ce7fde4b98691bb2
Tim <timevko@gmail.com>
2015-05-04 21:21:40 -0400

    troubleshooting chrome install

--- karma.conf.js
+++ karma.conf.js
@@ -57,12 +57,6 @@ module.exports = function(config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
 
-    customLaunchers: {
-        Chrome_travis_ci: {
-            base: 'Chrome',
-            flags: ['--no-sandbox']
-        }
-    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
